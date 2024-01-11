bd7db1849274b3f88ba55f391544c12ed76eb9f5
navyney <boontharika_kor@cmu.ac.th>
2024-01-11 15:29:07 +0700

    completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -2,43 +2,32 @@
 using namespace std;
 
 template <typename T>
-void insertionSort(T d[], int N)
-{
-	for (int i = 1; i < N; i++)
-	{
-		T temp = d[i];
-		for (int j = i - 1; j >= 0; j--)
-		{
-			if (d[j] < temp)
-			{
-				d[j + 1] = d[j];
-				if (j == 0)
-					d[j] = temp;
-			}
-			else
-			{
-				d[j + 1] = temp;
-				break;
+void insertionSort(T d[],int N){
+
+	for(int m = 1 ; m < N;m++){
+		for(int j = m ;j > 0 ; j--){
+			if(d[j] > d[j-1]){
+				T temp = d[j];
+				d[j] = d[j-1];
+				d[j-1] = temp;
 			}
 		}
-		cout << "Pass " << i << ":";
-		for (int k = 0; k < 10; k++)
-			cout << d[k] << " ";
+		cout << "Pass " << m << ":";
+		for(int o = 0; o < 10; o++) cout << d[o] << " ";	
 		cout << endl;
+
 	}
+
 }
 
-int main()
-{
-	int a[10] = {12, 25, 30, 44, 2, 0, 4, 7, 55, 25};
+int main(){
+	int a[10] = {12,25,30,44,2,0,4,7,55,25};
 	cout << "Input Array:";
-	for (int i = 0; i < 10; i++)
-		cout << a[i] << " ";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";
 	cout << "\n\n";
-
-	insertionSort(a, 10);
-
+	
+	insertionSort(a,10);
+	
 	cout << "\nSorted Array:";
-	for (int i = 0; i < 10; i++)
-		cout << a[i] << " ";
+	for(int i = 0; i < 10; i++) cout << a[i] << " ";	
 }
